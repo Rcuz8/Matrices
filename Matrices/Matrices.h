76ccdecf18b97f9d688e2c83c587457194f17269
Ryan Cocuzzo <rcocuzzo8@gmail.com>
2017-06-21 17:10:23 -0400

    Initial Commit

---
+++ Matrices/Matrices.h
@@ -0,0 +1,19 @@
+//
+//  Matrices.h
+//  Matrices
+//
+//  Created by Ryan Cocuzzo on 6/21/17.
+//  Copyright © 2017 rcocuzzo8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Matrices.
+FOUNDATION_EXPORT double MatricesVersionNumber;
+
+//! Project version string for Matrices.
+FOUNDATION_EXPORT const unsigned char MatricesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Matrices/PublicHeader.h>
+
+
